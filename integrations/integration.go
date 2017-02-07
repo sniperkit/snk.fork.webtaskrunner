674452d18cd1e9f54dc3c6101c3a20715bc83600
odog <odog@netcologne.de>
2017-02-08 00:34:01 +0100

    docs for the integration interface

--- integrations/integration.go
+++ integrations/integration.go
@@ -2,6 +2,7 @@ package integrations
 
 import "os/exec"
 
+//Integration defines the interface of a build system integration
 type Integration interface {
 	PrepareCommand(taskName string) *exec.Cmd
 	GetTaskList() []string
